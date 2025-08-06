5bc95b31cb8b2fcaca27d67b15216e8eaea458e7
javier parra <javierdaniloparra854@gmail.com>
2025-08-06 08:23:03 -0500

    correccion header responsive

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import './style.css'
+
+const btn_menu = document.getElementById('menu_toggle')
+const menu = document.getElementById('menu_cabecera')
+const menuLinks = menu.querySelectorAll('a');
+
+btn_menu.addEventListener('click', () => {
+  menu.classList.toggle('hidden')
+})
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    if (window.innerWidth < 768) {
+      menu.classList.add('hidden')
+    }
+  })
+})
